6d9f90b445a319e6dc60d8028af11307bcf7dff8
Joseph Hansel Daiz <joseph.hansel.daiz@gmail.com>
2024-11-06 17:02:51 +0800

    Remove console logs on database connection operations

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,27 +1,11 @@
 import mongoose from "mongoose";
 
-function connectToDatabase(uri: string): Promise<void> {
-  return mongoose
-    .connect(uri)
-    .then(() => {
-      console.log("Connected to MongoDB", uri);
-    })
-    .catch((error) => {
-      console.error("Failed to connect to MongoDB", error);
-      throw error;
-    });
+async function connectToDatabase(uri: string): Promise<void> {
+  await mongoose.connect(uri);
 }
 
 function disconnectFromDatabase(): Promise<void> {
-  return mongoose.connection
-    .close()
-    .then(() => {
-      console.log("Disconnected from MongoDB");
-    })
-    .catch((error) => {
-      console.error("Error disconnecting from MongoDB", error);
-      throw error;
-    });
+  return mongoose.connection.close();
 }
 
 export { connectToDatabase, disconnectFromDatabase };
